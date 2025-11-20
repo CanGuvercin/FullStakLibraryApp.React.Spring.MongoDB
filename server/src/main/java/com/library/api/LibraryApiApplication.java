c1189c185b72e7f4f7f750a71256a6b516380cbd
CanGuvercin <mustafacanguvercin@gmail.com>
2025-11-21 00:28:11 +0300

    loan is not working, forceby mode on Main class, testing

--- server/src/main/java/com/library/api/LibraryApiApplication.java
+++ server/src/main/java/com/library/api/LibraryApiApplication.java
@@ -1,13 +1,24 @@
+//package com.library.api;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class LibraryApiApplication {
+//	public static void main(String[] args) {
+//		SpringApplication.run(LibraryApiApplication.class, args);
+//	}
+//}
+
 package com.library.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.library.api")
 public class LibraryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
 	}
-
 }
